347276302ee90879f2a2866d7f1973c618537fbb
Sergio Khalil Bello Garcia <ikhalils@me.com>
2021-06-02 11:57:54 -0500

    Create the FeedItem model

---
+++ Feed/FeedItem.swift
@@ -0,0 +1,14 @@
+//
+//  FeedItem.swift
+//  Feed
+//
+//  Created by Sergio Khalil Bello Garcia on 02/06/21.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    var description, location: String?
+    var imageURL: URL
+}
