83bc307b166c2b90f427510e323c168b48cb6304
Sergio Khalil Bello Garcia <ikhalils@me.com>
2021-06-03 12:33:41 -0500

    Added singleton to assign a url to an static client

--- FeedTests/RemoteFeedLoaderTest.swift
+++ FeedTests/RemoteFeedLoaderTest.swift
@@ -7,21 +7,36 @@
 
 import XCTest
 class RemoteFeedLoader {
-    
+    func load() {
+        HTTPClient.shared.requestedURL = URL(string: "http://a-url.com")
+    }
 }
 
 class HTTPClient {
+    static let shared = HTTPClient()
+    
+    private init(){}
+    
     var requestedURL: URL?
 }
 
 class RemoteFeedLoaderTest: XCTestCase {
     
     func test_init_DoesNotRequestDateFromURL() {
-        let client = HTTPClient()
+        let client = HTTPClient.shared
 //        System Under Test (SUT)
         _ = RemoteFeedLoader()
         
         XCTAssertNil(client.requestedURL)
         
+    }
+    
+    func test_load_requestDataFromURL() {
+        let client = HTTPClient.shared
+        let sut = RemoteFeedLoader()
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
     }
 }
