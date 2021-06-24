7fb7551213f2c5ab092293341cccd190937474b8
Sergio Khalil Bello Garcia <ikhalils@me.com>
2021-06-24 11:58:12 -0500

    Move the creation of the `URLSessionHTTPClient` (System Under Test or SUT) to a factory method to protect our test from breaking changes

--- FeedTests/Feed API/URLSessionHTTPClientTest.swift
+++ FeedTests/Feed API/URLSessionHTTPClientTest.swift
@@ -50,7 +50,7 @@ class URLSessionHTTPClientTest: XCTestCase {
             exp.fulfill()
         }
         
-        URLSessionHTTPClient().get(from: url) {_ in}
+        makeSUT().get(from: url) {_ in}
         wait(for: [exp], timeout: 1.0)
         
         
@@ -60,10 +60,10 @@ class URLSessionHTTPClientTest: XCTestCase {
         let url = URL(string: "http://a-url.com")!
         let expectedError = NSError(domain: "Any error", code: 1)
         URLProtocolStub.stub(data: nil, response: nil, error: expectedError)
-        let sut = URLSessionHTTPClient()
+        
         
         let exp = expectation(description: "Wait for block")
-        sut.get(from: url) { result in
+        makeSUT().get(from: url) { result in
             switch result {
             case let .failure(receivedError):
                 // ios 14
@@ -80,6 +80,10 @@ class URLSessionHTTPClientTest: XCTestCase {
     }
     
 //   MARK :Helpers
+    
+    private func makeSUT() -> URLSessionHTTPClient {
+        return URLSessionHTTPClient()
+    }
     
     private class URLProtocolStub: URLProtocol {
         
