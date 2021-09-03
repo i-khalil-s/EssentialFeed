523a6d20cb3ddc7eda4d1acc50d0c398e3fe3fd3
Sergio Khalil Bello García <82245301+i-khalil-s@users.noreply.github.com>
2021-09-03 13:40:49 -0500

    Save command request cache deletion

--- FeedTests/Feed Cache/CacheFeedUseCaseTest.swift
+++ FeedTests/Feed Cache/CacheFeedUseCaseTest.swift
@@ -6,24 +6,59 @@
 //
 
 import XCTest
+import Feed
 
 class LocalFeedLoader {
+    
+    private let store: FeedStore
+    
     init(store: FeedStore) {
-        
+        self.store = store
+    }
+    
+    func save(items: [FeedItem]) {
+        store.deleteCachedFeed()
     }
 }
 
 class FeedStore {
     var deleteCachedFeedCallCount = 0
+    
+    func deleteCachedFeed() {
+        deleteCachedFeedCallCount += 1
+    }
 }
 
 class CacheFeedUseCaseTest: XCTestCase {
     
     func test_init_DoesNotDeleteCacheUponCreation() {
         let store = FeedStore()
-        
         _ = LocalFeedLoader(store: store)
         
         XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
+    }
+    
+    func test_save_requestCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        let items = [uniqueItem(), uniqueItem()]
+        
+        sut.save(items: items)
+        
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
+    }
+    
+    // MARK: - Helpers
+    
+    func uniqueItem() -> FeedItem {
+        return FeedItem(id: UUID(), description: anyString(), location: anyString(), imageURL: anyURL())
+    }
+    
+    private func anyURL() -> URL {
+        return URL(string: "http://a-url.com")!
+    }
+    
+    private func anyString() -> String {
+        return "any string"
     }
 }
