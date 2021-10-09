bdac892868977dab75a3ebb016b4ca0d4636d74e
Sergio Khalil Bello García <82245301+i-khalil-s@users.noreply.github.com>
2021-10-09 16:59:05 -0500

    Add comments to document that completions handlers in ant `FeedStore` implementation can be invoked in any thread. Clients are responsible to dispatch to appropriate threads, if needed.

--- Feed/Feed Cache/FeedStore.swift
+++ Feed/Feed Cache/FeedStore.swift
@@ -18,7 +18,15 @@ public protocol FeedStore {
     typealias InsertionCompletion = (Error?) -> Void
     typealias RetreivalCompletion = (RetreiveCacheFeedResult) -> Void
     
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to Dispatch to appropiate threads, if needed.
     func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to Dispatch to appropiate threads, if needed.
     func insert(feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion)
+    
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to Dispatch to appropiate threads, if needed.
     func retreive(completion: @escaping RetreivalCompletion)
 }
