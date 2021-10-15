160bfe2611fdcf10cf2791f3cc89249e72be7d15
Sergio Khalil Bello García <82245301+i-khalil-s@users.noreply.github.com>
2021-10-15 15:34:56 -0500

    Core Data Feed Store implementation

--- FeedTests/Feed Cache/CoreDataFeedStore.swift
+++ FeedTests/Feed Cache/CoreDataFeedStore.swift
@@ -8,10 +8,29 @@
 import XCTest
 import Feed
 
+class CoreDataFeedStore: FeedStore {
+    
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+        
+    }
+    
+    func insert(feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+        
+    }
+    
+    func retreive(completion: @escaping RetreivalCompletion) {
+        completion(.empty)
+    }
+    
+    
+}
+
 class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     
     func test_retrieve_deliversEmptyOnEmptyCache() {
-        
+        let sut = makeSUT()
+        assertThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
     }
     
     func test_retrieve_hasNoSideEffectsOnEmptyCache() {
@@ -60,5 +79,11 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     
     // MARK: Helpers
     
+    
+    private func makeSUT(storeURL: URL? = nil, file: StaticString = #filePath, line: UInt = #line) -> FeedStore {
+        let sut = CoreDataFeedStore()
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
+    }
     
 }
