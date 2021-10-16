cb64f5966d0f490fa73bf7c42dbe8933152d8fa5
Sergio Khalil Bello García <82245301+i-khalil-s@users.noreply.github.com>
2021-10-16 15:46:43 -0500

    Extract reusable `CoreData` helpers into a separate file

---
+++ Feed/Feed Cache/Infrastructure/CoreData/CoreDataHelpers.swift
@@ -0,0 +1,40 @@
+//
+//  CoreDataHelpers.swift
+//  Feed
+//
+//  Created by Sergio Khalil Bello Garcia on 16/10/21.
+//
+
+import CoreData
+
+internal extension NSPersistentContainer {
+    
+    enum LoadingModel: Swift.Error {
+        case modelNotFound
+        case failedToLoadPersistentStores(Swift.Error)
+    }
+    
+    static func load(modelName name: String, url: URL, in bundle: Bundle) throws -> NSPersistentContainer {
+        guard let model = NSManagedObjectModel.with(name: name, in: bundle) else {
+            throw LoadingModel.modelNotFound
+        }
+        
+        let description = NSPersistentStoreDescription(url: url)
+        let container = NSPersistentContainer(name: name, managedObjectModel: model)
+        container.persistentStoreDescriptions = [description]
+        
+        var loadError: Swift.Error?
+        container.loadPersistentStores { loadError = $1 }
+        try loadError.map { throw LoadingModel.failedToLoadPersistentStores($0)}
+        
+        return container
+    }
+}
+
+private extension NSManagedObjectModel {
+    static func with(name: String, in bundle: Bundle) -> NSManagedObjectModel? {
+        return bundle
+            .url(forResource: name, withExtension: "momd")
+            .flatMap { NSManagedObjectModel(contentsOf: $0) }
+    }
+}
