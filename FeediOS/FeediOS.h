4b72e04803099093a9b95d3a201301579f7d2863
Sergio Khalil Bello García <ikhalils@me.com>
2021-12-08 18:32:40 -0600

    Add `FeediOS` framework (prod and test) targets for iOS platform-specific components

---
+++ FeediOS/FeediOS.h
@@ -0,0 +1,18 @@
+//
+//  FeediOS.h
+//  FeediOS
+//
+//  Created by Sergio Khalil Bello Garcia on 08/12/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FeediOS.
+FOUNDATION_EXPORT double FeediOSVersionNumber;
+
+//! Project version string for FeediOS.
+FOUNDATION_EXPORT const unsigned char FeediOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FeediOS/PublicHeader.h>
+
+
