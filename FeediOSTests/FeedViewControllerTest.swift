fd98cfbdfecd363fc6bf2aeef2026bb44c2c4142
Sergio Khalil Bello García <ikhalils@me.com>
2021-12-21 18:23:42 -0600

    Does not load feed on init

---
+++ FeediOSTests/FeedViewControllerTest.swift
@@ -0,0 +1,32 @@
+//
+//  FeedViewControllerTest.swift
+//  FeediOSTests
+//
+//  Created by Sergio Khalil Bello Garcia on 21/12/21.
+//
+
+import Foundation
+import XCTest
+
+final class FeedViewController {
+    
+    init(loader: FeedViewControllerTest.LoaderSpy) {
+        
+    }
+}
+
+final class FeedViewControllerTest: XCTestCase {
+    
+    func test_init_doesNotLoadFeed() {
+        let loader = LoaderSpy()
+        let sut = FeedViewController(loader: loader)
+        
+        XCTAssertEqual(loader.loadCallCount, 0)
+    }
+    
+    // MARK: - Helpers
+    
+    class LoaderSpy {
+        private(set) var loadCallCount = 0
+    }
+}
