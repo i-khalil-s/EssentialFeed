b46a9eff1a592b2b7200a5803277a9abff16494e
Sergio Khalil Bello García <ikhalils@me.com>
2021-12-29 18:34:27 -0600

    Render loaded feed on successful load completion

---
+++ FeediOS/FeedImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageCell.swift
+//  FeediOS
+//
+//  Created by Sergio Khalil Bello Garcia on 28/12/21.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
