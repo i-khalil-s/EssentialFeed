568bcb01def1e6a655a7ef935b7f34d2a52b7fc4
Sergio Khalil Bello García <ikhalils@me.com>
2022-01-05 18:19:40 -0600

    Move `FeedImageDataLoader` to its own file

--- FeediOS/FeedViewController.swift
+++ FeediOS/FeedViewController.swift
@@ -8,15 +8,6 @@
 import UIKit
 import Feed
 
-public protocol FeedImageDataLoaderTask {
-    func cancel()
-}
-
-public protocol FeedImageDataLoader {
-    typealias Result = Swift.Result<Data, Error>
-    func loadImageData(from url: URL, completion: @escaping (Result) -> Void) -> FeedImageDataLoaderTask
-}
-
 final public class FeedViewController: UITableViewController {
     private var feedLoader: FeedLoader?
     private var tableModel = [FeedImage]()
