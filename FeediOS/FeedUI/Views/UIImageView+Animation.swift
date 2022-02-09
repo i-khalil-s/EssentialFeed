88759a6c1604a3da6790f477a992e1623cf13cad
Sergio Khalil Bello García <ikhalils@me.com>
2022-02-08 18:23:42 -0600

    Display image with animation

---
+++ FeediOS/FeedUI/Views/UIImageView+Animation.swift
@@ -0,0 +1,23 @@
+//
+//  UIImageView+Animation.swift
+//  FeediOS
+//
+//  Created by Sergio Khalil Bello Garcia on 08/02/22.
+//
+
+import UIKit
+
+extension UIImageView {
+    
+    func setImageAnimated(_ newImage: UIImage?) {
+        image = newImage
+        guard image != nil else {
+            return
+        }
+        
+        alpha = 0
+        UIView.animate(withDuration: 0.25) {
+            self.alpha = 1
+        }
+    }
+}
