f40e8806901e38c6cff6bd88abea01dec24ffed2
Sergio Khalil Bello García <ikhalils@me.com>
2022-05-12 18:26:30 -0500

    Duplicate `FeedUIComposer` as comments ui composer

---
+++ EssentialApp/EssentialApp/Composers/Comments/CommentsUIComposer.swift
@@ -0,0 +1,51 @@
+//
+//  CommentsUIComposer.swift
+//  EssentialApp
+//
+//  Created by Sergio Khalil Bello Garcia on 12/05/22.
+//
+
+import Combine
+import Feed
+import UIKit
+import FeediOS
+
+public final class CommentsUIComposer {
+    
+    private init () {}
+    
+    public static func commentsComposedWith(
+        commentsLoader: @escaping () -> FeedLoader.Publisher
+    ) -> ListViewController {
+        let presentationAdapter = LoadResourcePresentationAdapter<[FeedImage], FeedViewAdapter>(feedLoader: { commentsLoader().dispatchOnMainQueue()})
+        
+        let feedController = FeedUIComposer.makeWith(
+            title: FeedPresenter.title
+        )
+        
+        feedController.onRefresh = presentationAdapter.loadResource
+        
+        let presenter = LoadResourcePresenter(
+            resourceView: FeedViewAdapter(
+                controller: feedController,
+                loader: { _ in Empty<Data,Error>().eraseToAnyPublisher() }
+            ),
+            errorView: WeakRefVirtualProxy(feedController),
+            loadingView: WeakRefVirtualProxy(feedController),
+            mapper: FeedPresenter.map
+        )
+        presentationAdapter.presenter = presenter
+        return feedController
+    }
+}
+
+private extension FeedUIComposer {
+    static func makeWith(title: String) -> ListViewController {
+        let bundle = Bundle(for: ListViewController.self)
+        let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
+        let feedController = storyboard.instantiateInitialViewController() as! ListViewController
+        feedController.title = title
+        
+        return feedController
+    }
+}
---
+++ EssentialApp/EssentialAppTests/CommentsUIIntegrationTests.swift
@@ -0,0 +1,109 @@
+//
+//  CommentsUIIntegrationTests.swift
+//  EssentialAppTests
+//
+//  Created by Sergio Khalil Bello Garcia on 11/05/22.
+//
+
+import Foundation
+import XCTest
+import Feed
+import FeediOS
+import EssentialApp
+
+final class CommentsUIIntegrationTests: FeedUIIntegrationTests {
+    
+    override func test_feedView_hasTitle() {
+        let (sut, _) = makeSUT()
+        
+        sut.loadViewIfNeeded()
+        
+        XCTAssertEqual(sut.title, feedTitle)
+        
+    }
+    
+    override func test_loadFeedActions_requestFeedFromLoader() {
+        let (sut, loader) = makeSUT()
+        
+        XCTAssertEqual(loader.loadFeedCallCount, 0, "Expected no loading request before view is loaded")
+        
+        sut.loadViewIfNeeded()
+        XCTAssertEqual(loader.loadFeedCallCount, 1, "Expected a loading request once view is loaded")
+        
+        sut.simulateUserInitiatedFeedRealod()
+        XCTAssertEqual(loader.loadFeedCallCount, 2, "Expected another loading request once user initiates a load")
+        
+        sut.simulateUserInitiatedFeedRealod()
+        XCTAssertEqual(loader.loadFeedCallCount, 3, "Expected a thrid loading request once user initiates another load")
+    }
+    
+    override func test_loadingFeedIndicator_isVisibleWhileLoadingTheFeed() {
+        let (sut, loader) = makeSUT()
+        
+        sut.loadViewIfNeeded()
+        XCTAssertTrue(sut.isShowingLoadingIndicator, "Expected loading indicator once view is loaded")
+    
+        loader.compleFeedLoading(at: 0)
+        XCTAssertFalse(sut.isShowingLoadingIndicator, "Expected no loading indicator once loading is completed successfully")
+        
+        sut.simulateUserInitiatedFeedRealod()
+        XCTAssertTrue(sut.isShowingLoadingIndicator, "Expected loading indicator once user initiates a load")
+    
+        loader.compleFeedLoadingWithError(at: 1)
+        XCTAssertFalse(sut.isShowingLoadingIndicator, "Expected no loading indicator once user initiated loading is not completed")
+    }
+    
+    override func test_loadFeedCompletion_dispatchesFromBackgroundToMainThread() {
+        let (sut, loader) = makeSUT()
+        sut.loadViewIfNeeded()
+        
+        let expectation = expectation(description: "Wait for background queue")
+        DispatchQueue.global().async {
+            loader.compleFeedLoading(at: 0)
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 1.0)
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: ListViewController, loader: LoaderSpy) {
+        let loader = LoaderSpy()
+        let sut = CommentsUIComposer.commentsComposedWith(commentsLoader: loader.loadPublisher)
+        
+        trackForMemoryLeaks(loader, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        
+        return (sut, loader)
+    }
+    
+    private func assertThat(_ sut: ListViewController, hasViewConfiguredFor image: FeedImage, at index: Int, file: StaticString = #file, line: UInt = #line) {
+        let view = sut.feedImageView(at: index)
+        
+        guard let cell = view as? FeedImageCell else {
+            return XCTFail("Expected \(FeedImageCell.self) instance, but got \(String(describing: view)) instead", file: file, line: line)
+        }
+        
+        let shouldLocationBeVisible = (image.location != nil)
+        XCTAssertEqual(cell.isShowingLocation, shouldLocationBeVisible, "Expected `isShowingLocation` to be \(shouldLocationBeVisible) for image at \(index)", file: file, line: line)
+        
+        XCTAssertEqual(cell.locationText, image.location, "Expected `locationText` to be \(String(describing: image.location)) for image at \(index)", file: file, line: line)
+        
+        XCTAssertEqual(cell.descriptionText, image.description, "Expected `isShowingLocation` to be \(String(describing: image.description)) for image at \(index)", file: file, line: line)
+    }
+    
+    private func assertThat(_ sut: ListViewController, isRendering feed: [FeedImage], file: StaticString = #file, line: UInt = #line) {
+        sut.tableView.layoutIfNeeded()
+        RunLoop.main.run(until: Date())
+        guard sut.numberOfRenderedFeedImageViews() == feed.count else {
+            return XCTFail("Expected \(feed.count) images count, but got \(sut.numberOfRenderedFeedImageViews()) instead.", file: file, line: line)
+        }
+        
+        feed.enumerated().forEach { index, image in
+            assertThat(sut, hasViewConfiguredFor: image, at: index)
+        }
+    }
+    
+}
+
